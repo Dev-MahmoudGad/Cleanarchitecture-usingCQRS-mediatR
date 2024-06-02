0d49bd7d7e2ffd4e41505f403272f263431a2a1c
Mahmoud Gad <dev.mahmoudgad@hotmail.com>
2024-06-02 04:52:33 +0300

    Add files via upload

---
+++ Clean-Architecture/CleanArchitecture.Api/Controllers/CityController.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchitecture.Appllication.Features.City.Queries.GetCitiesList;
+using CleanArchitecture.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CityController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetAllCities")]
+        public async Task<ActionResult<GetCitiesListViewModel>> GetAllCitiesByGovernorateIdAsync(Guid GovernorateId)
+        {
+            var getEventDetailQuery = new GetCitiesListQuery(); //{ GovernorateId = GovernorateId };
+            return Ok(await _mediator.Send(getEventDetailQuery));
+        }
+    }
+}
