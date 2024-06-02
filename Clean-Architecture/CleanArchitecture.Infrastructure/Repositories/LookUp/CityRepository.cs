653d73b114be68ed5dbc7ed2ff13677c1b9310ca
Mahmoud Gad <dev.mahmoudgad@hotmail.com>
2024-06-03 00:57:16 +0300

    Add files via upload

---
+++ Clean-Architecture/CleanArchitecture.Infrastructure/Repositories/LookUp/CityRepository.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchitecture.Appllication.Features.Address.Queries.GetAddressesDetail;
+using CleanArchitecture.Domain.DTO.LookUp;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces.LookUp;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Repositories.LookUp
+{
+    public class CityRepository : BaseRepository<CityDto>, ICityRepository
+    {
+        public CityRepository(UsersDbContext postDbContext) : base(postDbContext)
+        {
+        }
+
+        public async Task<IReadOnlyList<CityDto>> GetAllCitiesAsync()
+        {
+            var cities = await _dbContext.Cities.Select(a => new CityDto { Id = a.Id, GovernorateId = a.GovernorateId, Name = a.Name }).AsNoTracking().ToListAsync();
+            return cities;
+        }
+
+        public async Task<IReadOnlyList<CityDto>> GetAllCitiesByGovernorateIdAsync(Guid GovernorateId)
+        {
+            var cities = await _dbContext.Cities.Where(a => a.GovernorateId == GovernorateId).Select(a => new CityDto { Id = a.Id, Name = a.Name }).AsNoTracking().ToListAsync();
+            return cities;
+        }
+
+    }
+}
---
+++ Clean-Architecture/CleanArchitecture.Infrastructure/Repositories/LookUp/GovernorateRepository.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitecture.Appllication.Features.Address.Queries.GetAddressesDetail;
+using CleanArchitecture.Domain.DTO.LookUp;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces.LookUp;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Repositories.LookUp
+{
+    public class GovernorateRepository : BaseRepository<GovernorateDto>, IGovernorateRepository
+    {
+        public GovernorateRepository(UsersDbContext postDbContext) : base(postDbContext)
+        {
+        }
+
+        public async Task<IReadOnlyList<GovernorateDto>> GetAllGovernorateAsync()
+        {
+            var governorates = await _dbContext.Governorates.AsNoTracking().Select(a => new GovernorateDto { Id = a.Id, Name = a.Name }).ToListAsync();
+            return governorates;
+        }
+    }
+}
